b19ab9cd13a1c39b2c0619c01410456358a14d3c
Suresh <cmsuresh@zohomail.in>
2022-12-24 11:05:39 +0530

    Added maven deps and liquibase plugin with different profiles

---
+++ src/main/resources/db/changelog/changelog-v1.mssql.sql
@@ -0,0 +1,8 @@
+-- liquibase formatted sql
+
+-- changeset Suresh:1671015692502-1
+CREATE TABLE employee (id INTEGER, name VARCHAR2(50), email VARCHAR2(50));
+
+-- changeset Suresh:1671015692502-2
+CREATE TABLE test_liq (id INTEGER, name VARCHAR2(10));
+
